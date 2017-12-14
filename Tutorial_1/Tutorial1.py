a8f3b572b583159c099ba1fe91550e16a79059ff
Anomy <allison.allain@gmail.com>
2017-12-13 20:53:35 -0500

    evaluation vs training

--- Tutorial_1/Tutorial1.py
+++ Tutorial_1/Tutorial1.py
@@ -1,3 +1,4 @@
+'''
 from __future__ import print_function
 
 import tensorflow as tf
@@ -52,3 +53,29 @@ for i in range(1000):
     sess.run(train, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]})
 
 print(sess.run([W, b])) #[array([-0.9999969], dtype=float32), array([ 0.99999082], dtype=float32)]
+'''
+
+
+############################
+
+import numpy as np
+import tensorflow as tf
+
+feature_colummns = [tf.feature_column.numeric_column("x", shape=[1])]
+estimator = tf.estimator.LinearRegressor(feature_columns=feature_colummns)
+
+
+x_train = np.array([1., 2., 3., 4.])
+y_train = np.array([0., -1., -2., -3.])
+x_eval = np.array([2., 5., 8., 1.])
+y_eval = np.array([-1.01, -4.1, -7, 0.])
+input_fn = tf.estimator.inputs.numpy_input_fn({"x": x_train}, y_train, batch_size=4, num_epochs=None, shuffle=True)
+train_input_fn = tf.estimator.inputs.numpy_input_fn({"x": x_train}, y_train, batch_size=4, num_epochs=1000, shuffle=False)
+eval_input_fn = tf.estimator.inputs.numpy_input_fn({"x": x_eval}, y_eval, batch_size=4, num_epochs=1000, shuffle=False)
+
+estimator.train(input_fn=input_fn, steps=1000)
+
+train_metrics = estimator.evaluate(input_fn=train_input_fn)
+eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
+print("train metrics: %r"% train_metrics)
+print("eval metrics: %r"% eval_metrics)
