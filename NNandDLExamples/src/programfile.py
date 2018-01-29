dfafb1b21c85d80753c2b51800bda74597ecf1bb
Anomy <allison.allain@gmail.com>
2018-01-29 13:52:24 -0500

    notes for Julian/Avi/Allison group

--- NNandDLExamples/src/programfile.py
+++ NNandDLExamples/src/programfile.py
@@ -1,4 +1,5 @@
 import mnist_loader
 print(mnist_loader)
+# TODO: magictypes JULIAN AVI ALLISON
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 print(training_data[0])
