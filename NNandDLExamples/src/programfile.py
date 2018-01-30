bc8a589e4d370242e4966c0d9eed37ff1c55e00f
Anomy <allison.allain@gmail.com>
2018-01-30 06:28:22 -0500

    exercises chap 1

--- NNandDLExamples/src/programfile.py
+++ NNandDLExamples/src/programfile.py
@@ -1,5 +1,19 @@
 import mnist_loader
-print(mnist_loader)
+import network
+
+# print(mnist_loader)
 # TODO: magictypes JULIAN AVI ALLISON
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-print(training_data[0])
+# print(training_data[0])
+
+# # Exercise 1 -- < 5 minutes
+# net = network.Network([784, 30, 10])
+# net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+# Exercise 2 -- 8 minutes
+# net = network.Network([784, 10])
+# net.SGD(training_data, 100, 10, 0.5, test_data=test_data)
+
+# Exercise 3 --
+net = network.Network([784, 10])
+net.SGD(training_data, 600, 10, 0.50, test_data=test_data)
