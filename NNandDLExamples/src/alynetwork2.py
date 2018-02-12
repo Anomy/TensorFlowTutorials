51fd645f2688920a58707f80f42338515857290a
Anomy <allison.allain@gmail.com>
2018-02-11 22:44:16 -0500

    over halfway with chapter 3

---
+++ NNandDLExamples/src/alynetwork2.py
@@ -0,0 +1,195 @@
+#### Libraries
+# Standard library
+import json
+import random
+import sys
+
+# Third-party libraries
+import numpy as np
+
+
+class QuadraticCost(object):
+
+    @staticmethod
+    def fn(a, y): # computation of hte quadratic cost associated to the actual output (a) and desired output (y)
+        return 0.5*np.linalg.norm(a-y)**2
+
+    def delta(z, a, y): # based on expression (30) for the output error for the quadratic cost
+        return (a-y) * sigmoid_prime(z)
+
+class CrossEntropyCost(object):
+
+    @staticmethod
+    def fn(a, y):
+        # np.nan_to_num ensures that Numpy deals correctly with the log of numbers very close to zero
+        return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
+
+    @staticmethod
+    def delta(z, a, y): # how to compute the output error
+        return (a-y)
+
+
+class Network(object):
+
+    def __init__(self, sizes, cost=CrossEntropyCost):
+        self.num_layers = len(sizes) # len() is just like .count
+        self.sizes = sizes
+        self.default_weight_initializer() #uses the 1/(num input weights^(1/2)) standard deviation
+        self.cost=cost
+
+    def default_weight_initializer(self): #uses the 1/(num input weights^(1/2)) standard deviation
+        self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
+        self.weights = [np.random.randn(y, x)/np.sqrt(x)
+                        for x, y in zip(self.sizes[:-1], self.sizes[1:])]
+
+    def large_weight_initializer(self): #uses the standard deviation of 1, really though, don't use this
+        self.biases = [np.random.rand(y, 1) for y in self.sizes[1:]]
+        self.weights = [np.random.rand(y, x)
+                        for x, y in zip(self.sizes[:-1], self.sizes[1:])]
+
+    def feedforward(self, a): #return output of network if (a) input
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a)+b)
+        return a
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta, lmbda = 0.0, evaluation_data=None, monitor_evaluation_cost=False, monitor_evaluation_accuracy=False, monitor_training_cost=False, monitor_training_accuracy=False):
+        if evaluation_data: n_data = len(evaluation_data)
+        n = len(training_data)
+        evaluation_cost, evaluation_accuracy = [], []
+        training_cost, training_accuracy = [], []
+        for j in xrange(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in xrange(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(
+                    mini_batch, eta, lmbda, len(training_data))
+            print "Epoch %s training complete" % j
+            if monitor_training_cost:
+                cost = self.total_cost(training_data, lmbda)
+                training_cost.append(cost)
+                print "Cost on training data: {}".format(cost)
+            if monitor_training_accuracy:
+                accuracy = self.accuracy(training_data, convert=True)
+                training_accuracy.append(accuracy)
+                print "Accuracy on training data: {} / {}".format(accuracy, n)
+            if monitor_evaluation_cost:
+                cost = self.total_cost(evaluation_data, lmbda, convert=True)
+                evaluation_cost.append(cost)
+                print "Cost on evaluation data: {}".format(cost)
+            if monitor_evaluation_accuracy:
+                accuracy = self.accuracy(evaluation_data)
+                evaluation_accuracy.append(accuracy)
+                print "Accuracy on evaluation data: {} / {}".format(self.accuracy(evaluation_data), n_data)
+            print
+        return evaluation_cost, evaluation_accuracy, training_cost, training_accuracy
+
+    # 'minibatch' is a list of tuples (x, y), 'eta' is learning rate, 'lmbda' is
+    # regularization parameter, n is size of training data set
+    def update_mini_batch(self, mini_batch, eta, lmbda, n):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        # L1 Regularization ## TODO BRAEK THIS ALLISON I CANT TELL IF WE ARE MISSING A W IN THE FIRST BIT OF THIS
+        # it's training VERY badly so there's probably something wrong.
+
+        # 
+        self.weights = [(np.sign(-w) * (eta * (lmbda / n))) * w - (eta / len(mini_batch)) * nw
+                        for w, nw in zip(self.weights, nabla_w)]
+
+        self.biases = [b - (eta / len(mini_batch)) * nb
+                       for b, nb in zip(self.biases, nabla_b)]
+
+        # L2 Regularization
+        # self.weights = [(1 - eta * (lmbda / n)) * w - (eta / len(mini_batch)) * nw
+        #                 for w, nw in zip(self.weights, nabla_w)]
+        # self.biases = [b - (eta / len(mini_batch)) * nb
+        #                for b, nb in zip(self.biases, nabla_b)]
+
+    # returns tuple nabla_b, nabla_w, representing the gradient
+    # for the cost function C_x 'nabla_b and nabla_w are layer-by-layer lists
+    # of numpy arrays, similar
+    # to self.biases and self.weights
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # feedforward
+        activation = x
+        activations = [x]
+        zs = [] #store all the z vectors
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        # backward
+        delta = (self.cost).delta(zs[-1], activations[-1], y)
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        for l in xrange(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+
+    def accuracy(self, data, convert=False):
+        if convert:
+            results = [(np.argmax(self.feedforward(x)), np.argmax(y))
+                       for (x, y) in data]
+        else:
+            results = [(np.argmax(self.feedforward(x)), y)
+                       for (x, y)  in data]
+        return sum(int(x == y) for (x, y) in results)
+
+
+    def total_cost(self, data, lmbda, convert=False):
+        cost = 0.0
+        for x, y in data:
+            a = self.feedforward(x)
+            if convert: y = vectorized_result(y)
+            cost += self.cost.fn(a, y)/len(data)
+        cost += 0.5*(lmbda/len(data))*sum(np.linalg.norm(w)**2 for w in self.weights)
+        return cost
+
+    def save(self, filename):
+        data = {"sizes": self.sizes,
+                "weights": [w.tolist() for w in self.weights],
+                "biases": [b.tolist() for b in self.biases],
+                "cost": str(self.cost.__name__)}
+        f = open(filename, "w")
+        json.dump(data, f)
+        f.close()
+
+
+
+
+
+
+def load(filename):
+    f = open(filename, "r")
+    data = json.load(f)
+    f.close()
+    cost = getattr(sys.modules[__name__], data["cost"])
+    net = Network(data["sizes"], cost=cost)
+    net.weights = [np.array(w) for w in data["weights"]]
+    net.biases = [np.array(b) for b in data["biases"]]
+    return net
+
+def vectorized_result(j): # returns 10-D unit vector with 1.0 in the jj'th position and zeroes elsewhere.
+    # used to convert a digit (0...9) inot corresponding desired output from the ANN
+    e = np.zeros((10, 1))
+    e[j] = 1.0
+    return e
+
+def sigmoid(z):
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z): # derivative of sigmoid
+    return sigmoid(z) * (1-sigmoid(z))
--- NNandDLExamples/src/programfile.py
+++ NNandDLExamples/src/programfile.py
@@ -1,9 +1,11 @@
 import mnist_loader
-import network
+# import network #exercise 1, 2, 3
+# import network2 #exercise 4-10
+import alynetwork2
 
 # print(mnist_loader)
 # TODO: magictypes JULIAN AVI ALLISON
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+# training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 # print(training_data[0])
 
 # # Exercise 1 -- < 5 minutes
@@ -15,5 +17,76 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 # net.SGD(training_data, 100, 10, 0.5, test_data=test_data)
 
 # Exercise 3 --
-net = network.Network([784, 10])
-net.SGD(training_data, 600, 10, 0.50, test_data=test_data)
+# net = network.Network([784, 10])
+# net.SGD(training_data, 600, 10, 0.50, test_data=test_data)
+# Epoch 599: 7438 / 10000 .. terrible, obviously.
+
+# Exercise 4 -- Chapter 3
+# Cross-entropy to classify MNIST digits
+# training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+# net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+# net.large_weight_initializer()
+# net.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data, monitor_evaluation_accuracy=True)
+
+# # Exercise 5 -- Chapter 3
+# training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+# net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+# net.large_weight_initializer()
+# net.SGD(training_data[:1000], 400, 10, 0.5, evaluation_data=test_data, monitor_evaluation_accuracy=True, monitor_training_cost=True)
+# # Epoch 399 training complete
+# # Cost on training data: 0.00359133710628
+# # Accuracy on evaluation data: 8191 / 10000
+
+# # Exercise 6 -- Chapter 3
+# training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+# net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+# net.large_weight_initializer()
+# net.SGD(training_data[:1000], 400, 10, 0.5, evaluation_data=test_data, lmbda = 0.1, monitor_evaluation_cost=True, monitor_evaluation_accuracy=True, monitor_training_cost=True, monitor_training_accuracy=True)
+# # Epoch 399 training complete
+# # Cost on training data: 0.11593782793
+# # Accuracy on training data: 1000 / 1000
+# # Cost on evaluation data: 0.846376388988
+# # Accuracy on evaluation data: 8786 / 10000
+
+# # Exercise 7 -- Chapter 3
+# training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+# net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+# net.large_weight_initializer()
+# net.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data, lmbda = 5.0, monitor_evaluation_accuracy=True, monitor_training_accuracy=True)
+# # Epoch 29 training complete
+# # Accuracy on training data: 48525 / 50000
+# # Accuracy on evaluation data: 9622 / 10000
+
+# # Exercise 8 -- Chapter 3
+# training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+# net = network2.Network([784, 100, 10], cost=network2.CrossEntropyCost)
+# net.large_weight_initializer()
+# net.SGD(training_data, 30, 10, 0.5, evaluation_data=validation_data, lmbda = 5.0, monitor_evaluation_accuracy=True)
+# # Epoch 29 training complete
+# # Accuracy on evaluation data: 9776 / 10000
+
+# # Exercise 9 -- Chapter 3 (Gaussian random normalized mean 0, standard deviation of 1)
+# training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+# net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+# net.large_weight_initializer()
+# net.SGD(training_data, 30, 10, 0.1, evaluation_data=validation_data, lmbda = 5.0, monitor_evaluation_accuracy=True)
+# # Epoch 29 training complete
+# # Accuracy on evaluation data: 9608 / 10000
+
+# # Exercise 10 -- Chapter 3  (Gaussian random normalized mean 0, standard deviation of 1/((n(in))^(1/2))...
+# # ... one over the square root of the number of number of input weights.
+# # note that we dropped "large_weight_initializer()" to do that.
+# training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+# net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+# net.SGD(training_data, 30, 10, 0.1, evaluation_data=validation_data, lmbda = 5.0, monitor_evaluation_accuracy=True)
+# # Epoch 19 training complete
+# # Accuracy on evaluation data: 9610 / 10000
+# # Epoch 25 training complete
+# # Accuracy on evaluation data: 9640 / 10000
+# # Epoch 29 training complete
+# # Accuracy on evaluation data: 9618 / 10000
+
+# Exercise 11 -- Chapter 3 -- now using alynetwork2
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net = alynetwork2.Network([784, 30, 10], cost=alynetwork2.CrossEntropyCost)
+net.SGD(training_data, 30, 10, 0.5, lmbda=5.0, evaluation_data=validation_data, monitor_evaluation_accuracy=True, monitor_evaluation_cost=True, monitor_training_accuracy=True, monitor_training_cost=True)
